de296b58c18091072a57d1cf1ade7396c0ea8a15
msheezi <marshall.randolph@gmail.com>
2019-12-01 14:50:22 -0800

    add extra payment calc function, working properly now

--- src/entry.js
+++ src/entry.js
@@ -44,17 +44,20 @@ export const Entry = () => {
         let totalPay = 0
         
         
+        
         for( let i = 0;i< iterations;i++){
            
                 let compoundInt = (1+ monthInt )**(remainingMonths-i)
                 let multiplier = (monthInt * compoundInt) / (compoundInt - 1)
                 let payment = Math.round(multiplier * calcPrinciple * 100) /100 
                 let interest = Math.round(calcPrinciple * monthInt * 100) / 100
+                
                 let principlePay = Math.round((payment - interest) * 100) /100
             if (payment > calcPrinciple){
                 payment = calcPrinciple 
                 interest = Math.round(calcPrinciple * monthInt * 100) / 100
                 principlePay = Math.round((payment - interest) * 100) / 100
+
                 calcPrinciple = Math.round((calcPrinciple - payment) * 100) / 100
                 totalInt += interest
                 totalPay += payment
@@ -82,6 +85,67 @@ export const Entry = () => {
         //    return <AmortLayout props={data}/>
     }
 
+    const runCalsExtra = (vals) => {
+        //add function to combine all the extra payments by index number and sum the 
+        //extra payments by index number to add into formula
+        //adjust payments to check index and add in the payment value, be sure to add in 
+        //the payments for display
+        // duplicate function to compare
+        // add summary to show the extra payments, reduced number of months and savings
+
+        let iterations = vals.duration
+        let monthInt = parseFloat(vals.interest) / 1200.0
+        let calcPrinciple = vals.principle
+        let remainingMonths = vals.duration
+        let totalInt = 0
+        let totalPay = 0
+        let extra = parseFloat(vals.extraPay)
+        let compoundInt = (1 + monthInt) ** (remainingMonths )
+        let multiplier = (monthInt * compoundInt) / (compoundInt - 1)
+        let calcPayment = Math.round(multiplier * calcPrinciple * 100) / 100
+        
+        
+        for (let i = 0; i < iterations; i++) {
+            if (calcPrinciple === 0){
+                return data
+            }
+            let interest = Math.round(calcPrinciple * monthInt * 100) / 100
+            let displayPayment =  Math.round((calcPayment + extra)*100) /100
+            
+            let principlePay = Math.round(((calcPayment - interest )+ extra )* 100) / 100
+            if (calcPayment > calcPrinciple) {
+                displayPayment = calcPrinciple
+                calcPayment = calcPrinciple
+                interest = Math.round(calcPrinciple * monthInt * 100) / 100
+                principlePay = Math.round((displayPayment - interest) * 100) / 100
+
+                calcPrinciple = Math.round((calcPrinciple - calcPayment) * 100) / 100
+                totalInt += interest
+                totalPay += displayPayment
+                
+            } else {
+                
+                calcPrinciple = Math.round((calcPrinciple - principlePay) * 100) / 100
+                totalInt += interest
+                totalPay += displayPayment
+            }
+
+            data[i] = { displayPayment, interest, principlePay, calcPrinciple }
+           
+        }
+        console.log(data)
+        console.log(extra)
+        totalInt = formatter.format(totalInt)
+        totalPay = formatter.format(totalPay)
+
+        setInterest(totalInt)
+        setPayments(totalPay)
+        // setState(data)
+        // console.log(state)
+        return data
+        //    return <AmortLayout props={data}/>
+    }
+
    
 
    const renderAmort = (state) => {
@@ -91,7 +155,7 @@ export const Entry = () => {
            return (
         <>
             <div className="month" key={key}>{parseInt(key) + 1}</div>
-            <div >{formatter.format(state[key].payment)}</div>
+            <div >{formatter.format(state[key].displayPayment)}</div>
             <div >{formatter.format(state[key].interest )}</div>
             <div >{formatter.format(state[key].principlePay )}</div>
             <div >{formatter.format(state[key].calcPrinciple )}</div>
@@ -120,10 +184,10 @@ export const Entry = () => {
                 <input type='text' name="duration" value={values.duration} onChange={handleChange}></input>
             </label>
             <label> Extra Monthly Payments:
-                <input type='text' name="extrapay" value={values.extraPay} onChange={handleChange}></input>
+                <input type='text' name="extraPay" value={values.extraPay} onChange={handleChange}></input>
             </label>
         </div>
-            <button onClick={() => setState(runCals(values)) }>Calculate</button>
+            <button onClick={() => setState(runCalsExtra(values)) }>Calculate</button>
             <div>Total Interest Paid: {interest ? interest : ""} </div>
             <div>Total Payments: {payments ? payments: ""} </div>
             <br/>
